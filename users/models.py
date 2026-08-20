bf6ec3df52ae74d7b35ab5c7950c40fa6035d60e
ssofia08 <sof.silva2008@gmail.com>
2026-08-20 18:59:00 -0300

    Crear el modelo de datos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    ROLES = (
+        ('consumidor', 'Consumidor'),
+        ('comercio', 'Comercio'),
+        ('organizacion', 'Organización'),
+    )
+    rol = models.CharField(max_length=20, choices=ROLES, default='consumidor')
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+
