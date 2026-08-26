f6c69cd969c438003412b59efe8fa6be11ae8396
Trini2007 <trinidadbossi@gmail.com>
2026-08-26 14:27:58 -0300

    Creo la base del catalogo de productos en HTML

--- scr/bd_lup/products/views.py
+++ scr/bd_lup/products/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
-from scr.bd_lup.products.models import Producto
+from .models import Producto  # Importa el modelo Producto de ESTA misma app
+
+def lista_productos(request):
+    # Filtramos los productos que estén marcados como DISPONIBLES
+    productos = Producto.objects.filter(estado='DISPONIBLE')
+    return render(request, 'catalogo_productos.html', {'productos': productos})
